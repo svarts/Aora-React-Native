b985e619a57e2c48db1c4a48aeb58b3e3504a1ca
Esma Karaca <killbillbeatrixx@gmail.com>
2024-04-12 23:48:48 +0300

    fix: Install and configure expo-router to resolve plugin issues.

---
+++ app/index.jsx
@@ -0,0 +1,17 @@
+import { StatusBar } from "expo-status-bar";
+import { Redirect, router } from "expo-router";
+import { View, Text, Image, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Welcome = () => {
+    return (
+        <SafeAreaView>
+            <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+                Where Creativity Meets Innovation: Embark on a Journey of Limitless
+                Exploration with Aora
+            </Text>
+        </SafeAreaView>
+    )
+};
+
+export default Welcome;
